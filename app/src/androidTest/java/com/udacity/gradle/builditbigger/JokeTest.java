4465f87c168884c74a7195db905a25052212a339
Yaseen <yaseen2591@gmail.com>
2016-07-12 01:49:15 +0530

    Added Connected Tests to verify AsynTask

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeTest.java
@@ -0,0 +1,37 @@
+package com.udacity.gradle.builditbigger;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Yaseen on 12/07/16.
+ */
+public class JokeTest extends ApplicationTestCase<Application> implements OnJokeReceivedListener {
+    CountDownLatch signal;
+    String joke;
+
+    public JokeTest() {
+        super(Application.class);
+    }
+
+    public void testJoke() {
+        try {
+            signal = new CountDownLatch(1);
+            new EndpointsAsyncTask().execute(this);
+            signal.await(10, TimeUnit.SECONDS);
+            assertNotNull("joke is null", joke);
+            assertFalse("joke is empty", joke.isEmpty());
+        } catch (Exception ex) {
+            fail();
+        }
+    }
+
+    @Override
+    public void onReceived(String joke) {
+        this.joke = joke;
+        signal.countDown();
+    }
+}
